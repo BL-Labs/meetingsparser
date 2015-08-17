9cb97df34abe4d929dfc77c85ed9b64c5da607dc
Ben O'Steen <ben.osteen@bl.uk>
2015-08-17 16:44:50 +0100

    Adding the helper file, updated to read test data txt

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-import csv
+import csv, os
 
 def get_test_doc():
     with open("testgeoreferencedmeetings.csv", "r") as inputfile:
@@ -12,3 +12,13 @@ def get_test_doc():
             outputlist.append(row)
         print("Number of rows: {0} (Number with a lat/long: {1})".format(idx+1, g))
     return outputlist
+    
+def get_meeting_texts():
+    for text in os.listdir("meetingtexts"):
+        with open("meetingtexts/"+text, encoding="utf-8-sig", mode="r") as inp:
+            yield inp.read()
+            
+def get_negative_texts():
+    for text in os.listdir("negativetexts"):
+        with open("negativetexts/"+text, encoding="utf-8", mode="r") as inp:
+            yield inp.read()
