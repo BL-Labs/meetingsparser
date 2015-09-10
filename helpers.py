0057408ffd4009d1022c86bee9364d91ff627714
Katrina Navickas <k.navickas@herts.ac.uk>
2015-09-10 23:07:36 +0100

    added corrected geo-referenced data & regex experiments

--- helpers.py
+++ helpers.py
@@ -1,5 +1,53 @@
 import csv, os
 
+import re
+
+class DateFinder(object):
+    def __init__(self):
+        self.shortmap = {'jan':'January',
+                         'feb':'February',
+                         'mar':'March',
+                         'apr':'April',
+                         'may':'May',
+                         'jun':'June',
+                         'jul':'July',
+                         'june':'June',
+                         'july':'July',
+                         'aug':'August',
+                         'sep':'September',
+                         'sept':'September',
+                         'oct':'October',
+                         'nov':'November',
+                         'dec':'December'}
+    
+    def find(self, inputtext):
+        resp ={}
+        for name, x in self.funclist():
+            resp[name] = x(inputtext)
+        return resp
+    
+    def funclist(self):
+        return [('date_standard', self.date_standard),
+                ('date_short', self.date_short)]
+        
+    def date_standard(self, inputtext):
+        standard = "(?P<day>[0-9]+)(?:st|nd|rd|th) of (?P<month>[A-Za-z]+)"
+        match = re.search(standard, inputtext, re.I|re.MULTILINE|re.U)
+        if match != None:
+            found = match.groupdict()
+            return {'day':found['day'], 'month':found['month']}
+        else:
+            return
+        
+    def date_short(self, inputtext):
+        standard = "(?P<month>[jan|feb|mar|apr|may|jun|june|jul|july|aug|sep|sept|oct|nov|dec]{3,})\.?\s+(?P<day>[0-9]{1,2})"
+        match = re.search(standard, inputtext, re.I|re.MULTILINE|re.U)
+        if match != None:
+            found = match.groupdict()
+            return {'month':self.shortmap.get(found['month'].lower(), found['month']), 'day':found['day']}
+        else:
+            return
+
 def get_test_doc():
     with open("testgeoreferencedmeetings.csv", "r") as inputfile:
         doc = csv.DictReader(inputfile)
